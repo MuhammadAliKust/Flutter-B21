eb4ada05113f30f93e5eee3dad31b3930196ad03
Muhammad Ali <kustalics@gmail.com>
2025-06-02 10:41:44 +0500

    Network and Asset Image Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'asset_image.dart';
+import 'network_image.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,72 +13,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(
-            'First Screen',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          centerTitle: true,
-          leading: Icon(Icons.arrow_back, color: Colors.white, size: 30),
-          actions: [
-            Icon(Icons.notifications, color: Colors.white, size: 30),
-            Icon(Icons.shopping_cart, color: Colors.white, size: 30),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("First Text", style: TextStyle(fontSize: 40)),
-            SizedBox(height: 20),
-            Text("Second Text", style: TextStyle(fontSize: 40)),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.thumb_up_alt),
-                Icon(Icons.thumb_up_alt),
-                Column(
-                  children: [
-                    Icon(Icons.thumb_up_alt),
-
-                    Icon(Icons.thumb_up_alt),
-                    Icon(Icons.thumb_up_alt),
-                    Row(
-                      children: [
-                        Icon(Icons.thumb_up_alt),
-                        Icon(Icons.thumb_up_alt),
-                        Column(
-                          children: [
-                            Icon(Icons.thumb_up_alt),
-                            Icon(Icons.thumb_up_alt),
-                            Icon(Icons.thumb_up_alt),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-                Icon(Icons.thumb_up_alt),
-                Icon(Icons.thumb_up_alt),
-                Icon(Icons.thumb_up_alt),
-              ],
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("Ab "),
-          backgroundColor: Colors.blue,
-        ),
-      ),
+      home: AssetImageDemo(),
+      // home: Scaffold(
+      //   backgroundColor: Colors.yellow,
+      //   appBar: AppBar(
+      //     backgroundColor: Colors.blue,
+      //     title: Text(
+      //       'First Screen',
+      //       style: TextStyle(
+      //         fontSize: 30,
+      //         fontWeight: FontWeight.bold,
+      //         color: Colors.white,
+      //       ),
+      //     ),
+      //     centerTitle: true,
+      //     leading: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+      //     actions: [
+      //       Icon(Icons.notifications, color: Colors.white, size: 30),
+      //       Icon(Icons.shopping_cart, color: Colors.white, size: 30),
+      //     ],
+      //   ),
+      //   body: Column(
+      //     mainAxisAlignment: MainAxisAlignment.start,
+      //     children: [
+      //       Text("First Text", style: TextStyle(fontSize: 40)),
+      //       SizedBox(height: 20),
+      //       Text("Second Text", style: TextStyle(fontSize: 40)),
+      //       SizedBox(height: 20),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //         children: [
+      //           Icon(Icons.thumb_up_alt),
+      //           Icon(Icons.thumb_up_alt),
+      //           Column(
+      //             children: [
+      //               Icon(Icons.thumb_up_alt),
+      //
+      //               Icon(Icons.thumb_up_alt),
+      //               Icon(Icons.thumb_up_alt),
+      //               Row(
+      //                 children: [
+      //                   Icon(Icons.thumb_up_alt),
+      //                   Icon(Icons.thumb_up_alt),
+      //                   Column(
+      //                     children: [
+      //                       Icon(Icons.thumb_up_alt),
+      //                       Icon(Icons.thumb_up_alt),
+      //                       Icon(Icons.thumb_up_alt),
+      //                     ],
+      //                   ),
+      //                 ],
+      //               ),
+      //             ],
+      //           ),
+      //
+      //           Icon(Icons.thumb_up_alt),
+      //           Icon(Icons.thumb_up_alt),
+      //           Icon(Icons.thumb_up_alt),
+      //         ],
+      //       ),
+      //     ],
+      //   ),
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: () {},
+      //     child: Text("Ab "),
+      //     backgroundColor: Colors.blue,
+      //   ),
+      // ),
     );
   }
 }
