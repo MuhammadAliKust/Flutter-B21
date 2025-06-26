5d423e360a5d231bca9851d32d5a223989649057
Muhammad Ali <kustalics@gmail.com>
2025-06-26 11:16:09 +0500

    Single Selection Done

--- lib/list_view_demo.dart
+++ lib/list_view_demo.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ListViewDemo extends StatelessWidget {
-  const ListViewDemo({super.key});
+class ListViewDemo extends StatefulWidget {
+  ListViewDemo({super.key});
+
+  @override
+  State<ListViewDemo> createState() => _ListViewDemoState();
+}
+
+class _ListViewDemoState extends State<ListViewDemo> {
+  int selectedIndex = -1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("List View Demo")),
       body: ListView.builder(
-        itemCount: 5,
+        itemCount: 10,
         itemBuilder: (context, i) {
           return Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
             child: Card(
               elevation: 3,
-              color: Colors.blue,
+              color: selectedIndex == i ? Colors.blue : Colors.white,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
               child: ListTile(
+                onTap: () {
+                  selectedIndex = i;
+                  setState(() {});
+                },
                 leading: Icon(Icons.notifications),
-                title: Text("Notification Title"),
-                subtitle: Text("Notification Sub Title"),
+                title: Text("Loop Index: $i"),
+                subtitle: Text("Selected Index: $selectedIndex"),
                 trailing: Icon(Icons.arrow_forward),
               ),
             ),
