27618e95976ff7007d351ae44f99d8e0b465dcd7
Muhammad Ali <kustalics@gmail.com>
2025-07-15 11:29:34 +0500

    Stack Done

--- lib/stack.dart
+++ lib/stack.dart
@@ -9,9 +9,15 @@ class StackDemo extends StatelessWidget {
       appBar: AppBar(title: Text("Stack Demo")),
       body: Stack(
         children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+
+            child: Image.asset('assets/images/register.PNG'),
+          ),
           Container(height: 300, width: 300, color: Colors.blue),
           Container(height: 200, width: 200, color: Colors.yellow),
           Container(height: 100, width: 100, color: Colors.green),
+          CircularProgressIndicator(),
         ],
       ),
     );
